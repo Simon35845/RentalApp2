f844e7e67409a36a6cd452547e9e558425b458b3
Simon35845 <DrakeKingman1991@gmail.com>
2025-03-04 03:08:15 +0300

    commit 5.5 - написал UserDetailsServiceImpl

---
+++ src/main/java/itacademy/rentalapp2/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package itacademy.rentalapp2.service;
+
+import itacademy.rentalapp2.entity.RoleEntity;
+import itacademy.rentalapp2.entity.UserEntity;
+import itacademy.rentalapp2.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles().stream().map(RoleEntity::getName).toArray(String[]::new))
+                .build();
+    }
+}
