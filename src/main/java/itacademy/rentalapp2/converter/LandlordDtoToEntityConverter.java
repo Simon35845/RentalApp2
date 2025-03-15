140fbd7eaaf48cdf0a350c05b44fbd42d7a9a628
Simon35845 <DrakeKingman1991@gmail.com>
2025-03-15 10:33:53 +0300

    commit 9.3 - написал конвертеры для landlord

---
+++ src/main/java/itacademy/rentalapp2/converter/LandlordDtoToEntityConverter.java
@@ -0,0 +1,21 @@
+package itacademy.rentalapp2.converter;
+
+import itacademy.rentalapp2.dto.LandlordDto;
+import itacademy.rentalapp2.entity.LandlordEntity;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LandlordDtoToEntityConverter implements Converter<LandlordDto, LandlordEntity> {
+    @Override
+    public LandlordEntity convert(LandlordDto dto) {
+        return LandlordEntity.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .surname(dto.getSurname())
+                .patronymic(dto.getPatronymic())
+                .age(dto.getAge())
+                .email(dto.getEmail())
+                .build();
+    }
+}
